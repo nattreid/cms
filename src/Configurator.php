a2abb5cfdb98464aa7b6b51e05f7c753c45dc06c
Attreid <attreid@gmail.com>
2016-08-26 12:09:29 +0200

    nazev stranek

--- src/Configurator.php
+++ src/Configurator.php
@@ -24,6 +24,7 @@ use NAttreid\Crm\Model\Orm,
  * @property string $cookiePolicyLink link pro informace o pouzivani cookie
  * @property string $keywords klicova slova
  * @property string $description popis
+ * @property string $title nazev stranek (napr Netta.cz)
  * 
  * @author Attreid <attreid@gmail.com>
  */
