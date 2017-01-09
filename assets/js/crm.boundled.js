95e8527cee94250c983e8a58353f236780232f3b
Attreid <attreid@gmail.com>
2017-01-09 13:45:51 +0100

    vypnuti happy

--- assets/js/crm.boundled.js
+++ assets/js/crm.boundled.js
@@ -39349,7 +39349,7 @@ $.nette.ext('history', {
     return cs;
 
 }));
-var datagridFitlerMultiSelect, datagridSortable, datagridSortableTree;
+var datagridFitlerMultiSelect, datagridShiftGroupSelection, datagridSortable, datagridSortableTree;
 
 $(document).on('click', '[data-datagrid-confirm]:not(.ajax)', function(e) {
   if (!confirm($(e.target).closest('a').attr('data-datagrid-confirm'))) {
@@ -39416,6 +39416,62 @@ $(document).on('keydown', 'input[data-datagrid-manualsubmit]', function(e) {
     return $(this).closest('form').submit();
   }
 });
+
+datagridShiftGroupSelection = function() {
+  var last_checkbox;
+  last_checkbox = null;
+  return document.addEventListener('click', function(e) {
+    var checkboxes_rows, current_checkbox_row, el, event, i, ie, input, j, k, last_checkbox_row, last_checkbox_tbody, len, len1, len2, ref, ref1, results, row, rows;
+    ref = e.path;
+    for (i = 0, len = ref.length; i < len; i++) {
+      el = ref[i];
+      if ($(el).is('.col-checkbox') && last_checkbox && e.shiftKey) {
+        current_checkbox_row = $(el).closest('tr');
+        last_checkbox_row = last_checkbox.closest('tr');
+        last_checkbox_tbody = last_checkbox_row.closest('tbody');
+        checkboxes_rows = last_checkbox_tbody.find('tr').toArray();
+        if (current_checkbox_row.index() > last_checkbox_row.index()) {
+          rows = checkboxes_rows.slice(last_checkbox_row.index(), current_checkbox_row.index());
+        } else if (current_checkbox_row.index() < last_checkbox_row.index()) {
+          rows = checkboxes_rows.slice(current_checkbox_row.index() + 1, last_checkbox_row.index());
+        }
+        if (!rows) {
+          return;
+        }
+        for (j = 0, len1 = rows.length; j < len1; j++) {
+          row = rows[j];
+          input = $(row).find('.col-checkbox input[type=checkbox]')[0];
+          if (input) {
+            input.checked = true;
+            ie = window.navigator.userAgent.indexOf("MSIE ");
+            if (ie) {
+              event = document.createEvent('Event');
+              event.initEvent('change', true, true);
+            } else {
+              event = new Event('change', {
+                'bubbles': true
+              });
+            }
+            input.dispatchEvent(event);
+          }
+        }
+      }
+    }
+    ref1 = e.path;
+    results = [];
+    for (k = 0, len2 = ref1.length; k < len2; k++) {
+      el = ref1[k];
+      if ($(el).is('.col-checkbox')) {
+        results.push(last_checkbox = $(el));
+      } else {
+        results.push(void 0);
+      }
+    }
+    return results;
+  });
+};
+
+datagridShiftGroupSelection();
 
 document.addEventListener('change', function(e) {
   var at_least_one, event, grid, i, ie, input, inputs, len, results, select;
@@ -39964,7 +40020,7 @@ $.nette.ext('datagrid.reset-filter-by-column', {
       return grid.find('[data-datagrid-reset-filter-by-column]').each(function() {
         var new_href;
         key = $(this).attr('data-datagrid-reset-filter-by-column');
-        new_href = href.replace('do=examplesGrid-resetFilter', 'do=' + payload._datagrid_name + '-resetColumnFilter');
+        new_href = href.replace('do=' + payload._datagrid_name + '-resetFilter', 'do=' + payload._datagrid_name + '-resetColumnFilter');
         new_href += '&' + payload._datagrid_name + '-key=' + key;
         return $(this).attr('href', new_href);
       });
@@ -40006,208 +40062,6 @@ $(function() {
       url: $('.datagrid').first().data('refresh-state')
     });
   }
-});
-
-var Happy,
-  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-
-Happy = (function() {
-  function Happy() {
-    this.checkCheckboxState = bind(this.checkCheckboxState, this);
-    this.checkCheckboxStateOnChange = bind(this.checkCheckboxStateOnChange, this);
-    this.checkCheckboxStateOnClick = bind(this.checkCheckboxStateOnClick, this);
-    this.radioOnChange = bind(this.radioOnChange, this);
-    this.colors = ['primary', 'success', 'info', 'warning', 'danger', 'white', 'gray'];
-    this.templates = {
-      radio: '<div class="happy-radio"><b></b></div>',
-      checkbox: '<div class="happy-checkbox"> <svg viewBox="0 0 30 30"> <rect class="mark-storke" x="15" y="3" rx="1" ry="1" width="10" height="4"/> <rect class="mark-storke" x="-7" y="21" rx="1" ry="1" width="19" height="4"/> </svg> </div>',
-      text: '',
-      textarea: ''
-    };
-  }
-
-  Happy.prototype.colorify = function(input, happy_input, class_string) {
-    if (input.classList.contains(class_string)) {
-      happy_input.classList.add(class_string);
-    }
-    if (input.classList.contains(class_string + '-border')) {
-      return happy_input.classList.add(class_string + '-border');
-    }
-  };
-
-  Happy.prototype.thicknessify = function(input, happy_input) {
-    if (input.classList.contains('thin')) {
-      return happy_input.classList.add('thin');
-    }
-  };
-
-  Happy.prototype.setNames = function(input, happy_input) {
-    var value;
-    happy_input.setAttribute('data-name', input.getAttribute('name'));
-    value = input.getAttribute('value');
-    if (typeof (value !== 'undefined') && value !== false && value !== null) {
-      return happy_input.setAttribute('data-value', input.getAttribute('value'));
-    }
-  };
-
-  Happy.prototype.init = function() {
-    this.initRadio();
-    return this.initCheckbox();
-  };
-
-  Happy.prototype.removeBySelector = function(s) {
-    var el, elements, i, len, results;
-    elements = document.querySelectorAll(s);
-    results = [];
-    for (i = 0, len = elements.length; i < len; i++) {
-      el = elements[i];
-      results.push(el.parentNode.removeChild(el));
-    }
-    return results;
-  };
-
-  Happy.prototype.reset = function() {
-    this.removeBySelector('.happy-radio');
-    this.removeBySelector('.happy-checkbox');
-    return this.init();
-  };
-
-  Happy.prototype.initRadio = function() {
-    var c, happy_input, i, input, inputs, j, len, len1, ref, results;
-    inputs = document.querySelectorAll('input[type=radio].happy');
-    results = [];
-    for (i = 0, len = inputs.length; i < len; i++) {
-      input = inputs[i];
-      input.insertAdjacentHTML('afterend', this.templates.radio);
-      happy_input = input.nextElementSibling;
-      ref = this.colors;
-      for (j = 0, len1 = ref.length; j < len1; j++) {
-        c = ref[j];
-        this.colorify(input, happy_input, c);
-        this.setNames(input, happy_input);
-      }
-      this.thicknessify(input, happy_input);
-      this.checkRadioState(input);
-      results.push(document.addEventListener('change', this.radioOnChange));
-    }
-    return results;
-  };
-
-  Happy.prototype.radioOnChange = function(e) {
-    var happy_radio, happy_radios, i, len, name, selector, target_input;
-    target_input = e.target;
-    if (!target_input.classList.contains('happy')) {
-      return;
-    }
-    name = target_input.getAttribute('name');
-    selector = '.happy-radio[data-name="' + name + '"]';
-    happy_radios = document.querySelectorAll(selector);
-    for (i = 0, len = happy_radios.length; i < len; i++) {
-      happy_radio = happy_radios[i];
-      happy_radio.classList.remove('active');
-    }
-    return this.checkRadioState(target_input);
-  };
-
-  Happy.prototype.checkRadioState = function(input) {
-    var happy_radio, name, selector, value;
-    if (input.checked) {
-      name = input.getAttribute('name');
-      value = input.getAttribute('value');
-      selector = '.happy-radio[data-name="' + name + '"][data-value="' + value + '"]';
-      happy_radio = document.querySelector(selector);
-      if (happy_radio) {
-        return happy_radio.classList.add('active');
-      }
-    }
-  };
-
-  Happy.prototype.initCheckbox = function() {
-    var c, happy_input, i, input, inputs, j, len, len1, ref, results;
-    inputs = document.querySelectorAll('input[type=checkbox].happy');
-    results = [];
-    for (i = 0, len = inputs.length; i < len; i++) {
-      input = inputs[i];
-      input.insertAdjacentHTML('afterend', this.templates.checkbox);
-      happy_input = input.nextElementSibling;
-      ref = this.colors;
-      for (j = 0, len1 = ref.length; j < len1; j++) {
-        c = ref[j];
-        this.colorify(input, happy_input, c);
-        this.setNames(input, happy_input);
-      }
-      this.thicknessify(input, happy_input);
-      this.checkCheckboxState(input);
-      document.addEventListener('click', this.checkCheckboxStateOnClick);
-      results.push(document.addEventListener('change', this.checkCheckboxStateOnChange));
-    }
-    return results;
-  };
-
-  Happy.prototype.checkCheckboxStateOnClick = function(e) {
-    var event, happy_input, ie, input, selector;
-    if (e.target.tagName === 'svg') {
-      happy_input = e.target.parentNode;
-    } else if (e.target.tagName === 'rect') {
-      happy_input = e.target.parentNode.parentNode;
-    } else {
-      happy_input = e.target;
-    }
-    if (!happy_input || !happy_input.classList.contains('happy-checkbox')) {
-      return;
-    }
-    e.preventDefault();
-    selector = 'input[type=checkbox][name="' + happy_input.getAttribute('data-name') + '"]';
-    input = document.querySelector(selector);
-    if (input) {
-      if (happy_input.classList.contains('active')) {
-        input.checked = false;
-        happy_input.classList.remove('active');
-      } else {
-        input.checked = true;
-        happy_input.classList.add('active');
-      }
-      ie = window.navigator.userAgent.indexOf("MSIE ");
-      if (ie) {
-        event = document.createEvent('Event');
-        event.initEvent('change', true, true);
-      } else {
-        event = new Event('change', {
-          'bubbles': true
-        });
-      }
-      return input.dispatchEvent(event);
-    }
-  };
-
-  Happy.prototype.checkCheckboxStateOnChange = function(e) {
-    var input;
-    if (e.target.classList.contains('happy')) {
-      input = e.target;
-      return this.checkCheckboxState(input);
-    }
-  };
-
-  Happy.prototype.checkCheckboxState = function(input) {
-    var element, selector;
-    selector = '.happy-checkbox[data-name="' + input.getAttribute('name') + '"]';
-    element = document.querySelector(selector);
-    if (element) {
-      if (input.checked) {
-        return element.classList.add('active');
-      } else {
-        return element.classList.remove('active');
-      }
-    }
-  };
-
-  return Happy;
-
-})();
-
-document.addEventListener('DOMContentLoaded', function() {
-  window.happy = new Happy();
-  return window.happy.init();
 });
 
 /*!
--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ var boundledJS = [
     paths.dev.vendor + 'ublaboo-datagrid/assets/dist/datagrid.js',
     paths.dev.vendor + 'ublaboo-datagrid/assets/dist/datagrid-spinners.js',
     paths.dev.vendor + 'ublaboo-datagrid/assets/dist/datagrid-instant-url-refresh.js',
-    paths.dev.vendor + 'happy/dist/happy.js',
+    // paths.dev.vendor + 'happy/dist/happy.js',
     paths.dev.vendor + 'bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
     paths.dev.vendor + 'jquery-ui-sortable/jquery-ui-sortable.js',
     paths.dev.vendor + 'bootstrap-select/dist/js/bootstrap-select.js',
@@ -131,7 +131,7 @@ function getBoundledCSS() {
             // datagrid
             paths.dev.vendor + 'ublaboo-datagrid/assets/dist/datagrid.css',
             paths.dev.vendor + 'ublaboo-datagrid/assets/dist/datagrid-spinners.css',
-            paths.dev.vendor + 'happy/dist/happy.css',
+            // paths.dev.vendor + 'happy/dist/happy.css',
             paths.dev.vendor + 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.css',
             paths.dev.vendor + 'bootstrap-select/dist/css/bootstrap-select.css',
             // plugins
