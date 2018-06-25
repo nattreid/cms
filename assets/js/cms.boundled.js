9626dd5bd748debaadf69d69045099217eca9e68
attreid <attreid@gmail.com>
2018-06-25 13:52:37 +0200

    assets

--- assets/js/cms.boundled.js
+++ assets/js/cms.boundled.js
@@ -52119,17 +52119,37 @@ Nette.validators.NAttreidFormRules_validateImage = function (elem, arg, val) {
     if (window.jQuery === undefined) {
         console.error('Plugin "jQuery" required by "spectrum.js" is missing!');
         return;
+    } else if (window.moment === undefined) {
+        console.error('Plugin "moment.js" required by "spectrum.js" is missing!');
+        return;
     } else if (window.jQuery.fn.spectrum === undefined) {
         console.error('Plugin "Spectrum" required by "spectrum.js" is missing!');
         return;
     }
 
+    var localize = {
+        cs: {
+            choose: 'Vybrat',
+            cancel: 'Zrušit'
+        },
+        en: {
+            choose: 'Choose',
+            cancel: 'Cancel'
+        }
+    };
+
+    var locale = localize[window.moment.locale()];
+
     function spectrum() {
         $('input.spectrum').spectrum({
             color: $(this).val(),
             showAlpha: true,
+            showInitial: true,
             showInput: true,
-            preferredFormat: 'rgb'
+            allowEmpty: true,
+            preferredFormat: 'rgb',
+            chooseText: locale.choose,
+            cancelText: locale.cancel
         });
     }
 
